a0011532926c7b607b1ab252f7d0d1cf05958566
Adrian Zander <adrianzander06@gmail.com>
2023-05-26 10:05:03 +0200

    Masse endringer

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ function bil1(){
     document.getElementById('bilNavn').innerHTML = "<b>1992 Rocket Bunny<br>Honda NSX</b>";
     document.getElementById('bilLogo').src = "bilder/honda-automobiles-vector-logo.svg";
     document.getElementById('bilLogo').alt = "Honda logo";
+    document.getElementById('tekst').innerHTML = "En Rocket Bunny Honda NSX er en Honda NSX produsert i 1992 som har blitt veldig modifisert med et widebody kit fra den japanske produsenten Rocket Bunny. En Honda NSX har 270 hestekrefter og 7100 RPM. Bilen kan gjøre 0-100 km/t på 4,9 sekunder med en topp fart på 270 km/t."
 }
 
 function bil2(){
     document.getElementById('storBilde').src = "bilder/Mercedes AMG GT Black Series.png";
-    document.getElementById('bilNavn').innerHTML = "<b>2020 Mercedes-AMG<br>GT Black Series</b>";
+    document.getElementById('bilNavn').innerHTML = "<b>2021 Mercedes-AMG<br>GT Black Series</b>";
     document.getElementById('bilLogo').src = "bilder/Mercedes AMG.svg";
     document.getElementById('bilLogo').alt = "AMG logo";
+    document.getElementById('tekst').innerHTML = "Mercedes AMG GT Black Series er den kraftigste AMG bilen så langt med sin 4,0-liter twin-turbo V-8 motor som kan putte ut 720 hestekrefter. Bilen kan gjøre 0-100 km/t på bare 3,2 skunder og har en toppfart på over 320 km/t. Black Series har mer hestekrefter, veier mindre og er mer optimalisert for racing enn en vanlig AMG GT."
 }
 
 function bil3(){
@@ -17,4 +19,5 @@ function bil3(){
     document.getElementById('bilNavn').innerHTML = "<b>1999 Nissan R34 GTR</b>";
     document.getElementById('bilLogo').src = "bilder/gtr.svg";
     document.getElementById('bilLogo').alt = "Gtr logo";
+    document.getElementById('tekst').innerHTML = ""
 }
