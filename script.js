b818ef064226b5cc6812ec0a0c3e627190bef95f
Adrian Zander <adrianzander06@gmail.com>
2023-05-26 10:04:42 +0200

    Masse nytt

---
+++ script.js
@@ -0,0 +1,20 @@
+function bil1(){
+    document.getElementById('storBilde').src = "bilder/Rocket Bunny Honda NSX.png";
+    document.getElementById('bilNavn').innerHTML = "<b>1992 Rocket Bunny<br>Honda NSX</b>";
+    document.getElementById('bilLogo').src = "bilder/honda-automobiles-vector-logo.svg";
+    document.getElementById('bilLogo').alt = "Honda logo";
+}
+
+function bil2(){
+    document.getElementById('storBilde').src = "bilder/Mercedes AMG GT Black Series.png";
+    document.getElementById('bilNavn').innerHTML = "<b>2020 Mercedes-AMG<br>GT Black Series</b>";
+    document.getElementById('bilLogo').src = "bilder/Mercedes AMG.svg";
+    document.getElementById('bilLogo').alt = "AMG logo";
+}
+
+function bil3(){
+    document.getElementById('storBilde').src = "bilder/Skyline.svg";
+    document.getElementById('bilNavn').innerHTML = "<b>1999 Nissan R34 GTR</b>";
+    document.getElementById('bilLogo').src = "bilder/gtr.svg";
+    document.getElementById('bilLogo').alt = "Gtr logo";
+}
